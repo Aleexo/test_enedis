38d7ddbf6f2c9ae546e4f7fa6e58e98fe1a77a13
Aleexo <81173896+Aleexo@users.noreply.github.com>
2021-08-23 14:37:53 +0200

    Add files via upload

---
+++ js/options.js
@@ -0,0 +1,13 @@
+function options(getTypeNoeud, getTypeUse, colorn, colorl, getColorChoice, getNodeColor, getLinkColor, linkToolTip, nodeToolTip, getRadiusFunction, needLabel) {
+    this.getTypeNoeud = getTypeNoeud
+    this.getTypeUse = getTypeUse
+    this.colorn = colorn
+    this.colorl = colorl
+    this.getColorChoice = getColorChoice
+    this.getNodeColor = getNodeColor
+    this.getLinkColor = getLinkColor
+    this.linkToolTip = linkToolTip
+    this.nodeToolTip = nodeToolTip
+    this.getRadiusFunction = getRadiusFunction
+    this.needLabel = needLabel
+}
