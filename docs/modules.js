c755428c1be25ff8d79e274d0cc08746e821a64e
Aleexo <81173896+Aleexo@users.noreply.github.com>
2021-08-24 16:35:41 +0200

    Add files via upload

---
+++ docs/modules.js
@@ -0,0 +1,22 @@
+modules = [
+    "auscultationhta",
+    "cruecrise",
+    "datapreparation.bdp",
+    "datapreparation.bt",
+    "datapreparation.cim",
+    "datapreparation.cleaning",
+    "datapreparation.flood",
+    "datapreparation.hdr",
+    "datapreparation.hta",
+    "datapreparation.htabt",
+    "datapreparation.incident",
+    "datapreparation.land",
+    "datapreparation.weather",
+    "monitoringtransfo",
+    "previsioncanicule",
+    "recurringactivities",
+    "renouvellementbt",
+    "renouvellementhta",
+    "stormimpactmodeling",
+    "suivicrue"
+]
